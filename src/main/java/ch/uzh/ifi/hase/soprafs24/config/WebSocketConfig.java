476f480ba3f897226ad545133a0e481f05519898
Lorenzo Frigoli <lorenzofrigoli@Air-di-Lorenzo.fritz.box>
2025-04-15 23:27:16 +0200

    Fixed WebSocketConfig and tests
    Fixed web-socket configuration and commented out logging in
    GameSessionController. Restored tests for GameSessionController as they
    are now correctly working.

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -10,17 +10,16 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableSimpleBroker("/game/topic");
-    config.setApplicationDestinationPrefixes("/app");
-  }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/game/topic");
+    }
 
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws")
-      .setAllowedOrigins("http://localhost:3000")  // frontend URL
-      .withSockJS();
-  }
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/game-ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
+    }
 
 }
--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/GameSessionControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/GameSessionControllerTest.java
@@ -62,152 +62,152 @@ public class GameSessionControllerTest {
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
     }
 
-    // @Test
-    // public void testPlayerAction_invalidToken_receivesError() throws Exception {
-    //     // Mock user token
-    //     String token = "aaa";
+    @Test
+    public void testPlayerAction_invalidToken_receivesError() throws Exception {
+        // Mock user token
+        String token = "aaa";
 
-    //     // Configure the mock service to return false for token validation.
-    //     given(appService.isUserTokenValid(Mockito.any())).willReturn(false);
+        // Configure the mock service to return false for token validation.
+        given(appService.isUserTokenValid(Mockito.any())).willReturn(false);
 
-    //     // Connect to the WebSocket server.
-    //     String url = "ws://localhost:" + port + "/game-ws";
-    //     StompSession session = stompClient.connect(url, new StompSessionHandlerAdapter() {
-    //     }).get(1, TimeUnit.SECONDS);
+        // Connect to the WebSocket server.
+        String url = "ws://localhost:" + port + "/game-ws";
+        StompSession session = stompClient.connect(url, new StompSessionHandlerAdapter() {
+        }).get(1, TimeUnit.SECONDS);
 
-    //     // The stomp client will trigger the latch when the message is received.
-    //     final CountDownLatch errorReceived = new CountDownLatch(1);
-    //     // AtomicReference to store the error message.
-    //     final AtomicReference<String> errorMessageRef = new AtomicReference<>();
+        // The stomp client will trigger the latch when the message is received.
+        final CountDownLatch errorReceived = new CountDownLatch(1);
+        // AtomicReference to store the error message.
+        final AtomicReference<String> errorMessageRef = new AtomicReference<>();
 
-    //     // Subscribe to the user topic.
-    //     String userEndpoint = "/game/topic/user/" + token;
-    //     session.subscribe(userEndpoint, new StompFrameHandler() {
-    //         @Override
-    //         public Type getPayloadType(StompHeaders headers) {
-    //             return GameSessionErrorMessage.class;
-    //         }
+        // Subscribe to the user topic.
+        String userEndpoint = "/game/topic/user/" + token;
+        session.subscribe(userEndpoint, new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return GameSessionErrorMessage.class;
+            }
 
-    //         @Override
-    //         public void handleFrame(StompHeaders headers, Object payload) {
-    //             GameSessionErrorMessage errorMessage = (GameSessionErrorMessage) payload;
-    //             errorMessageRef.set(errorMessage.getErrorMessage());
-    //             errorReceived.countDown();
-    //         }
-    //     });
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                GameSessionErrorMessage errorMessage = (GameSessionErrorMessage) payload;
+                errorMessageRef.set(errorMessage.getErrorMessage());
+                errorReceived.countDown();
+            }
+        });
 
-    //     // Send a message with an invalid token in the headers.
-    //     StompHeaders headers = new StompHeaders();
-    //     headers.setDestination("/game/player-action");
-    //     headers.add("auth-token", token);
-    //     session.send(headers, new PlayerAction());
+        // Send a message with an invalid token in the headers.
+        StompHeaders headers = new StompHeaders();
+        headers.setDestination("/game/player-action");
+        headers.add("auth-token", token);
+        session.send(headers, new PlayerAction());
 
-    //     if (!errorReceived.await(3, TimeUnit.SECONDS)) {
-    //         fail("Expected error message was not received.");
-    //     }
-    //     String errorMsg = errorMessageRef.get();
-    //     assertNotNull("Error message should not be null", errorMsg);
-    //     assertTrue(errorMsg.contains("Invalid auth token"), "Unexpected error message");
-    // }
+        if (!errorReceived.await(3, TimeUnit.SECONDS)) {
+            fail("Expected error message was not received.");
+        }
+        String errorMsg = errorMessageRef.get();
+        assertNotNull("Error message should not be null", errorMsg);
+        assertTrue(errorMsg.contains("Invalid auth token"), "Unexpected error message");
+    }
 
-    // @Test
-    // public void testPlayerAction_succesful() throws Exception {
-    //     // given
-    //     String userToken = "abc";
-    //     String gsToken = "def";
-    //     PlayerAction playerAction = new PlayerAction();
-    //     playerAction.setGameSessionToken(gsToken);
+    @Test
+    public void testPlayerAction_succesful() throws Exception {
+        // given
+        String userToken = "abc";
+        String gsToken = "def";
+        PlayerAction playerAction = new PlayerAction();
+        playerAction.setGameSessionToken(gsToken);
 
-    //     given(appService.isUserTokenValid(userToken)).willReturn(true);
-    //     given(appService.isGameTokenValid(gsToken)).willReturn(true);
-    //     given(gameSessionService.handlePlayerAction(Mockito.any(), Mockito.any(), Mockito.any()))
-    //             .willReturn(new PlayerActionResult());
+        given(appService.isUserTokenValid(userToken)).willReturn(true);
+        given(appService.isGameTokenValid(gsToken)).willReturn(true);
+        given(gameSessionService.handlePlayerAction(Mockito.any(), Mockito.any(), Mockito.any()))
+                .willReturn(new PlayerActionResult());
 
-    //     // Connect to the WebSocket server.
-    //     String url = "ws://localhost:" + port + "/game-ws";
-    //     StompSession session = stompClient.connect(url, new StompSessionHandlerAdapter() {
-    //     }).get(1, TimeUnit.SECONDS);
+        // Connect to the WebSocket server.
+        String url = "ws://localhost:" + port + "/game-ws";
+        StompSession session = stompClient.connect(url, new StompSessionHandlerAdapter() {
+        }).get(1, TimeUnit.SECONDS);
 
-    //     // The stomp client will trigger the latch when the message is received.
-    //     final CountDownLatch messageReceived = new CountDownLatch(1);
-    //     // AtomicReference to store the PlayerActionResult.
-    //     final AtomicReference<PlayerActionResult> playerActionResult = new AtomicReference<>();
+        // The stomp client will trigger the latch when the message is received.
+        final CountDownLatch messageReceived = new CountDownLatch(1);
+        // AtomicReference to store the PlayerActionResult.
+        final AtomicReference<PlayerActionResult> playerActionResult = new AtomicReference<>();
 
-    //     // Subscribe to the game session topic.
-    //     String userEndpoint = "/game/topic/" + gsToken;
-    //     session.subscribe(userEndpoint, new StompFrameHandler() {
-    //         @Override
-    //         public Type getPayloadType(StompHeaders headers) {
-    //             return PlayerActionResult.class;
-    //         }
+        // Subscribe to the game session topic.
+        String userEndpoint = "/game/topic/" + gsToken;
+        session.subscribe(userEndpoint, new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return PlayerActionResult.class;
+            }
 
-    //         @Override
-    //         public void handleFrame(StompHeaders headers, Object payload) {
-    //             playerActionResult.set((PlayerActionResult) payload);
-    //             messageReceived.countDown();
-    //         }
-    //     });
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                playerActionResult.set((PlayerActionResult) payload);
+                messageReceived.countDown();
+            }
+        });
 
-    //     // Send a message with the given game session token.
-    //     StompHeaders headers = new StompHeaders();
-    //     headers.setDestination("/game/player-action");
-    //     headers.add("auth-token", userToken);
-    //     session.send(headers, playerAction);
+        // Send a message with the given game session token.
+        StompHeaders headers = new StompHeaders();
+        headers.setDestination("/game/player-action");
+        headers.add("auth-token", userToken);
+        session.send(headers, playerAction);
 
-    //     if (!messageReceived.await(3, TimeUnit.SECONDS)) {
-    //         fail("Expected message was not received.");
-    //     }
-    //     // Check if the PlayerActionResult is not null.
-    //     assertNotNull(playerActionResult.get(), "Message payload should not be null");
-    // }
+        if (!messageReceived.await(3, TimeUnit.SECONDS)) {
+            fail("Expected message was not received.");
+        }
+        // Check if the PlayerActionResult is not null.
+        assertNotNull(playerActionResult.get(), "Message payload should not be null");
+    }
 
-    // @Test
-    // public void testPlayerAction_service_error() throws Exception {
-    //     // Mock user token
-    //     String token = "aaa";
+    @Test
+    public void testPlayerAction_service_error() throws Exception {
+        // Mock user token
+        String token = "aaa";
 
-    //     given(appService.isUserTokenValid(token)).willReturn(true);
-    //     given(appService.isGameTokenValid(Mockito.any())).willReturn(true);
-    //     given(gameSessionService.handlePlayerAction(Mockito.any(), Mockito.any(), Mockito.any()))
-    //             .willThrow(new Exception("Service error"));
+        given(appService.isUserTokenValid(token)).willReturn(true);
+        given(appService.isGameTokenValid(Mockito.any())).willReturn(true);
+        given(gameSessionService.handlePlayerAction(Mockito.any(), Mockito.any(), Mockito.any()))
+                .willThrow(new Exception("Service error"));
 
-    //     // Connect to the WebSocket server.
-    //     String url = "ws://localhost:" + port + "/game-ws";
-    //     StompSession session = stompClient.connect(url, new StompSessionHandlerAdapter() {
-    //     }).get(1, TimeUnit.SECONDS);
+        // Connect to the WebSocket server.
+        String url = "ws://localhost:" + port + "/game-ws";
+        StompSession session = stompClient.connect(url, new StompSessionHandlerAdapter() {
+        }).get(1, TimeUnit.SECONDS);
 
-    //     // The stomp client will trigger the latch when the message is received.
-    //     final CountDownLatch errorReceived = new CountDownLatch(1);
-    //     // AtomicReference to store the error message.
-    //     final AtomicReference<String> errorMessageRef = new AtomicReference<>();
+        // The stomp client will trigger the latch when the message is received.
+        final CountDownLatch errorReceived = new CountDownLatch(1);
+        // AtomicReference to store the error message.
+        final AtomicReference<String> errorMessageRef = new AtomicReference<>();
 
-    //     // Subscribe to the user topic.
-    //     String userEndpoint = "/game/topic/user/" + token;
-    //     session.subscribe(userEndpoint, new StompFrameHandler() {
-    //         @Override
-    //         public Type getPayloadType(StompHeaders headers) {
-    //             return GameSessionErrorMessage.class;
-    //         }
+        // Subscribe to the user topic.
+        String userEndpoint = "/game/topic/user/" + token;
+        session.subscribe(userEndpoint, new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return GameSessionErrorMessage.class;
+            }
 
-    //         @Override
-    //         public void handleFrame(StompHeaders headers, Object payload) {
-    //             GameSessionErrorMessage errorMessage = (GameSessionErrorMessage) payload;
-    //             errorMessageRef.set(errorMessage.getErrorMessage());
-    //             errorReceived.countDown();
-    //         }
-    //     });
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                GameSessionErrorMessage errorMessage = (GameSessionErrorMessage) payload;
+                errorMessageRef.set(errorMessage.getErrorMessage());
+                errorReceived.countDown();
+            }
+        });
 
-    //     // Send a message with an invalid token in the headers.
-    //     StompHeaders headers = new StompHeaders();
-    //     headers.setDestination("/game/player-action");
-    //     headers.add("auth-token", token);
-    //     session.send(headers, new PlayerAction());
+        // Send a message with an invalid token in the headers.
+        StompHeaders headers = new StompHeaders();
+        headers.setDestination("/game/player-action");
+        headers.add("auth-token", token);
+        session.send(headers, new PlayerAction());
 
-    //     if (!errorReceived.await(3, TimeUnit.SECONDS)) {
-    //         fail("Expected error message was not received.");
-    //     }
-    //     String errorMsg = errorMessageRef.get();
-    //     assertNotNull("Error message should not be null", errorMsg);
-    //     assertTrue(errorMsg.contains("Service error"), "Unexpected error message");
-    // }
+        if (!errorReceived.await(3, TimeUnit.SECONDS)) {
+            fail("Expected error message was not received.");
+        }
+        String errorMsg = errorMessageRef.get();
+        assertNotNull("Error message should not be null", errorMsg);
+        assertTrue(errorMsg.contains("Service error"), "Unexpected error message");
+    }
 }
